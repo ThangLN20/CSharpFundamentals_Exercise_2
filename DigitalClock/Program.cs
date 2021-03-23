553751583f66b3bae746b428e2d05e311b61294e
ThangLN20 <81183522+ThangLN20@users.noreply.github.com>
2021-03-23 23:59:27 +0700

    Add files via upload

---
+++ DigitalClock/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockPublisher clockPublisher = new ClockPublisher();
+            ClockSubcriber clockSubscriber = new ClockSubcriber();
+            clockSubscriber.Subcriber(clockPublisher);
+
+            clockPublisher.Run();
+        }
+    }
+}
